a6fc1148962315fae4c220254603bf6030b5b10e
HZXhuang <1966752024@qq.com>
2023-11-03 00:29:55 +0800

    搜索页面调整

--- code/frontend/src/components/user/Search.vue
+++ code/frontend/src/components/user/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div style="margin-bottom: 20px">
-      <el-select style="width: 150px; margin-right: 10px" v-model="search_data.area" placeholder="请选择地区">
+      <el-select clearable style="width: 150px; margin-right: 10px" v-model="search_data.area" placeholder="请选择地区">
         <el-option
             v-for="(item,index) in options.area"
             :key="index"
@@ -9,7 +9,7 @@
             :value="item">
         </el-option>
       </el-select>
-      <el-select style="width: 180px; margin-right: 10px" v-model="search_data.postTime" placeholder="请选择公布时间">
+      <el-select clearable style="width: 180px; margin-right: 10px" v-model="search_data.postTime" placeholder="请选择公布时间">
         <el-option
             v-for="(item,index) in options.postTime"
             :key="index"
