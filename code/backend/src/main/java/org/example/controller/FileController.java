f6ec2fdff3bf914a7fff110bc1a5de57813ca8b1
YangT-ao <1728326545@qq.com>
2023-11-02 17:46:45 +0800

    补充接口相关信息

--- code/backend/src/main/java/org/example/controller/FileController.java
+++ code/backend/src/main/java/org/example/controller/FileController.java
@@ -3,6 +3,8 @@ package org.example.controller;
 import cn.hutool.json.JSON;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.service.FileService;
@@ -15,6 +17,7 @@ import java.io.IOException;
 /**
  * @author hzx
  */
+@Tag(name="文件",description = "文件相关接口")
 @RestController
 @RequestMapping("/files")
 @Api(tags = "文件上传下载控制器")
@@ -30,6 +33,7 @@ public class FileController {
      * @return 响应结果
      * @throws IOException IO异常
      */
+    @Operation(summary = "上传", description = "上传文件到本地接口")
     @PostMapping("/upload")
     @ApiOperation(value = "上传文件到本地接口")
     public Result upload(MultipartFile file) throws IOException {
@@ -43,6 +47,7 @@ public class FileController {
      * @return JSON对象
      * @throws IOException 写入文件失败
      */
+    @Operation(summary = "上传", description = "富文本文件上传接口")
     @PostMapping("/editor/upload")
     @ApiOperation(value = "富文本文件上传接口")
     public JSON editorUpload(MultipartFile file) throws IOException {
@@ -54,6 +59,7 @@ public class FileController {
      * @param flag 文件标识
      * @param response http响应对象
      */
+    @Operation(summary = "下载", description = "文件下载接口")
     @GetMapping("/get/{flag}")
     @ApiOperation(value = "文件下载接口")
     public void getFiles(@PathVariable String flag, HttpServletResponse response) throws IOException {
--- code/backend/src/main/java/org/example/controller/OfflineActivityController.java
+++ code/backend/src/main/java/org/example/controller/OfflineActivityController.java
@@ -1,28 +1,33 @@
 package org.example.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.pojo.OfflineActivity;
 import org.example.service.OfflineActivityService;
 import org.example.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name="线下活动",description = "线下活动相关接口")
 @RestController
 public class OfflineActivityController {
 
     @Autowired
     OfflineActivityService offlineActivityService;
 
+    @Operation(summary = "查询", description = "查询所有审批通过的线下活动")
     @GetMapping("/getAllOfflineActivity")
     public Result getAllOfflineActivity(){
 
         return offlineActivityService.getAllOfflineActivity();
     }
 
+    @Operation(summary = "查询", description = "分页查询所有审批通过的线下活动")
     @GetMapping("getAllOfflineActivityByPage")
     public Result getAllOfflineActivityByPage(@RequestParam("pageSize") Integer pageSize, @RequestParam("pageNumber") Integer pageNumber){
         return offlineActivityService.getAllOfflineActivityByPage(pageSize,pageNumber);
     }
-
+    @Operation(summary = "申请", description = "用户申请线下宣传活动")
     @PostMapping("/activityPromotionApply")
     public Result activityPromotionApply(@RequestBody OfflineActivity offlineActivity){
         return offlineActivityService.activityPromotionApply(offlineActivity);
