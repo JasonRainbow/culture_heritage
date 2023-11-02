2060d6a541cb97612f05c03cb74b35af5ffb5214
HZXhuang <1966752024@qq.com>
2023-11-02 08:18:55 +0800

    前端设置代理

--- code/frontend/vue.config.js
+++ code/frontend/vue.config.js
@@ -1,5 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: process.env.NODE_ENV !== "production"
+  lintOnSave: process.env.NODE_ENV !== "production",
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        pathRewrite:{'^/api':''},
+        // ws: true,//用于支持websocket，默认值为true
+        // changeOrigin: true //用于控制请求头中的host值，默认值为true
+      },
+    }
+  }
 })
